ca33edf136adc4a2ae1c99e58469f18d50670dee
Duke Nukem <garrettduke87@gmail.com>
2020-08-15 15:31:59 -0600

    added all the questions

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,8 @@
 // Assignment code here
+
+// welcome alert
+window.alert("Welcome to the random password generator.")
+
 var generateBtn = document.querySelector("#generate");
 let password = "";
 let masterArray = [];
@@ -6,9 +10,24 @@ let lowerArray = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m
 let capArray = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`, `I`, `J`, `K`, `L`, `M`, `N`, `O`, `P`, `Q`, `R`, `S`, `T`, `U`, `V`, `W`, `X`, `Y`, `Z`];
 let symbolArray = [`!`, `@`, `#`, `$`, `%`, `^`, `&`, `*`, `-`, `.`, `?`];
 let numberArray = [`1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `0`];
-// welcome alert
-window.alert("Welcome to the random password generator.")
 
+
+function valNumber () {
+  do {
+    // lower and upper case letters
+    var numLength = window.prompt(`Please choose the length of your password. Choose a number between 8 and 128`);
+    numLength = parseInt(numLength);
+
+    var lowerCase = window.confirm(`Would you like to use lowercase letters in your password?`);
+
+    var upperCase = window.confirm(`Would you like to use uppercase letters in your password?`);
+    
+    // symbols and numbers
+    var useSymbols = window.confirm(`Would you like to use symbols in your password?`);
+    var useNumbers = window.confirm(`Would you like to use numbers in your password?`);
+
+  }
+}
 // prompt for length of pw
 
 // prompt for character types, uppercase or lowercase to use in pw
