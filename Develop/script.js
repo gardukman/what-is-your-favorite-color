3670ed02fb368edeca99b10984cefac78c995320
Duke Nukem <garrettduke87@gmail.com>
2020-08-15 15:12:31 -0600

    added array variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,23 @@
 // Assignment code here
+var generateBtn = document.querySelector("#generate");
+let password = "";
+let masterArray = [];
+let lowerArray = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`];
+let capArray = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`, `I`, `J`, `K`, `L`, `M`, `N`, `O`, `P`, `Q`, `R`, `S`, `T`, `U`, `V`, `W`, `X`, `Y`, `Z`];
+let symbolArray = [`!`, `@`, `#`, `$`, `%`, `^`, `&`, `*`, `-`, `.`, `?`];
+let numberArray = [`1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `0`];
+// welcome alert
+window.alert("Welcome to the random password generator.")
 
+// prompt for length of pw
+
+// prompt for character types, uppercase or lowercase to use in pw
+
+//prompt for char types, symbols
+
+
+
+var length = prompt("Please choose a number between 8 and 128.");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
